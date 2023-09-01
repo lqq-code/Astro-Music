c07087a85a1bb885ddf895ee654c26341ba5eacb
choco <choco.luo@playerauctions.com>
2023-09-01 15:51:29 +0800

    modify route

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -10,7 +10,7 @@ export default function Music({ musics }: any){
 					{
 						musics.map((post) => (
 							<li key={post.id} >
-								<a href={`/music/${post.id}/`}>
+								<a href={`/${post.id}/`}>
 									<img width={230} height={230} src={post.heroImage} alt="" />
 									<div className="musicTitle">{post.title}</div>
 									<div className="musicDes">{post.description}</div>
