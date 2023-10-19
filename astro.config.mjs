e69594c53b1d6e19bfaf20f0a7677271219c3e98
choco <choco.luo@playerauctions.com>
2023-10-19 14:53:51 +0800

    feat: add gsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
 	integrations: [react()],
-	// adapter: node({
-	// 	mode: 'standalone',
-	// }),
+	adapter: node({
+		mode: 'standalone',
+	}),
 	adapter: vercel(),
  	output: 'server',
 });
--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,23 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
+
 import './Music.css';
 
-export default function Music({ musics }: any){
+
+
+export default function Music({ musics }: any) {
+	const circle = useRef();
+
+	function animateOnScroll() {
+		const elements = document.querySelectorAll(".music-item");
+
+		elements.forEach((element) => {
+			const animation = gsap.from(element, {
+				opacity: 0,
+				y: 100,
+				duration: 0.8,
+				paused: true,
+			});
+
+			window.addEventListener("scroll", () => {
+				const scrollPosition = window.scrollY;
+				const elementPosition = (element as HTMLElement).offsetTop;
+				if (scrollPosition > elementPosition - window.innerHeight / 2) {
+					animation.play();
+				} else {
+					animation.reverse();
+				}
+			});
+		});
+	}
+	const onEnterImg = ({ currentTarget }) => {
+		gsap.to(currentTarget, { scale: 0.95, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('img'), { scale: 1.2, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('.musicTitle'), { scale: 1.2, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('.musicDes'), { scale: 1.2, duration: 0.3 });
+	};
+
+	const onLeaveImg = ({ currentTarget }) => {
+		gsap.to(currentTarget, { scale: 1, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('img'), { scale: 1, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('.musicTitle'), { scale: 1, duration: 0.3 });
+		gsap.to(currentTarget.querySelector('.musicDes'), { scale: 1, duration: 0.3 });
+	};
+
+
+	useEffect(() => {
+		animateOnScroll();
+	}, []);
 
 	return (
 		<>
-			<div  className="music">
-			<ul>
-					{
-						musics.map((post) => (
-							<li key={post.id} >
-								<a href={`/${post.id}/`}>
-									<img width={220} height={220} src={post.heroImage} alt="" />
-									<div className="musicTitle">{post.title}</div>
-									<div className="musicDes">{post.description}</div>
-								</a>
-							</li>
-						))
-					}
+			<div className="music">
+				<ul>
+					{musics.map((post) => (
+						<li key={post.id} className="music-item" onMouseEnter={onEnterImg} onMouseLeave={onLeaveImg} >
+							<a href={`/${post.id}/`} >
+								<img width={400} height={400} src={post.heroImage} alt="" />
+								<div className="musicTitle">{post.title}</div>
+								<div className="musicDes">{post.description}</div>
+							</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</>
