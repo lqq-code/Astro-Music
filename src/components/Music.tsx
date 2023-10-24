adf9917aabade37f5151b053f7b7e96e0b287d6f
choco <choco.luo@playerauctions.com>
2023-10-24 14:01:34 +0800

    updated: animation

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -33,7 +33,7 @@ export default function Music({ musics }: any) {
 	const onEnterImg = ({ currentTarget }) => {
 		gsap.to(currentTarget, { scale: 0.8, duration: 0.5 });
 		gsap.to(currentTarget.querySelector('img'), { scale: 1.2, duration: 0.5 });
-		gsap.to(currentTarget.querySelector('.musicTitle'), { scale: 1.2, duration: 0.5 });
+		gsap.to(currentTarget.querySelector('.musicTitle'), { scale: 1.5, duration: 0.5 });
 		gsap.to(currentTarget.querySelector('.musicDes'), { scale: 1.2, duration: 0.5 });
 	};
 
@@ -52,17 +52,19 @@ export default function Music({ musics }: any) {
 	return (
 		<>
 			<div className="music">
+				
 				<ul>
 					{musics.map((post) => (
 						<li key={post.id} className="music-item" onMouseEnter={onEnterImg} onMouseLeave={onLeaveImg} >
 							<a href={`/${post.id}/`} >
-								<img width={400} height={400} src={post.heroImage} alt="" />
+								<img src={post.heroImage} alt="" className="revealing-image"/>
 								<div className="musicTitle">{post.title}</div>
 								<div className="musicDes">{post.description}</div>
 							</a>
 						</li>
 					))}
 				</ul>
+				
 			</div>
 		</>
 	);
